7417291457cb286150bdc17e605be36fe531cdc6
Susana Pham <s.susana.pham@gmail.com>
2022-12-05 11:05:04 +0100

    Fix

--- webserver.js
+++ webserver.js
@@ -1,13 +1,17 @@
+const path = require('path')
 const http = require('http')
 const smor = require('smor')
 
 const PORT = 8080
 
+const dir = path.join(process.argv[0], '..', 'dist')
+
 const server = http.createServer((req, res) => {
-  smor(req, res, {
-    dir: 'dist'
-  })
+  smor(req, res, { dir })
 })
 
-console.log(`\nStarted web server.\n\nGo to http://localhost:${PORT}`)
+console.log(`\nServing files from ${dir}`)
+console.log(`\nGo to http://localhost:${PORT}`)
+console.log(`\nCreated by Eldøy Projects`)
+
 server.listen(PORT)
